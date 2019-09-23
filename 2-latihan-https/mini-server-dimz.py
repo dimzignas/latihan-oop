9e586cdbe48f2073448f929852086c7c3f031106
Ignatius Dimas <ignatius.dimas92@gmail.com>
2019-09-23 13:17:38 +0700

    perubahan dan perbaikan metode (versi 2.1)

--- 2-latihan-https/mini-server-dimz.py
+++ 2-latihan-https/mini-server-dimz.py
@@ -5,11 +5,36 @@ with some modification by dimz
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
+import ssl
 # import inspect
 
 
 # ------Class Cases------------------------------
-class case_no_file(object):
+class base_case(object):
+    '''Parent for case handlers.'''
+
+    # Handle file
+    def handle_file(self, handler, full_path):
+        try:
+            # sementara hanya bisa buka send text file
+            with open(full_path, 'r') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(full_path, msg)
+            handler.handle_error(msg)
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        assert False, 'Not implemented.'
+
+    def act(self, handler):
+        assert False, 'Not implemented.'
+
+
+class case_no_file(base_case):
     '''File or directory does not exist.'''
 
     def test(self, handler):
@@ -19,17 +44,39 @@ class case_no_file(object):
         raise ServerException("'{0}' not found".format(handler.full_path))
 
 
-class case_existing_file(object):
+class case_cgi_file(base_case):
+    '''Something runnable.'''
+
+    def test(self, handler):
+        return os.path.isfile(handler.full_path) and \
+               handler.full_path.endswith('.py')
+
+    def act(self, handler):
+        handler.run_cgi(handler.full_path)
+
+
+class case_existing_file(base_case):
     '''File exists.'''
 
     def test(self, handler):
         return os.path.isfile(handler.full_path)
 
     def act(self, handler):
-        handler.handle_file(handler.full_path)
+        self.handle_file(handler, handler.full_path)
 
 
-class case_directory_no_index_file(object):
+class case_directory_index_file(base_case):
+    '''Serve index.html page for a directory.'''
+
+    def test(self, handler):
+        return os.path.isdir(handler.full_path) and \
+            os.path.isfile(self.index_path(handler))
+
+    def act(self, handler):
+        self.handle_file(handler, self.index_path(handler))
+
+
+class case_directory_no_index_file(base_case):
     '''Serve listing for a directory without an index.html page.'''
 
     def index_path(self, handler):
@@ -43,21 +90,7 @@ class case_directory_no_index_file(object):
         handler.list_dir(handler.full_path)
 
 
-class case_directory_index_file(object):
-    '''Serve index.html page for a directory.'''
-
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
-
-    def test(self, handler):
-        return os.path.isdir(handler.full_path) and \
-            os.path.isfile(self.index_path(handler))
-
-    def act(self, handler):
-        handler.handle_file(self.index_path(handler))
-
-
-class case_always_fail(object):
+class case_always_fail(base_case):
     '''Base case if nothing else worked.'''
 
     def test(self, handler):
@@ -94,6 +127,7 @@ class RequestHandler(BaseHTTPRequestHandler):
     '''
 
     Cases = [case_no_file,
+             case_cgi_file,
              case_existing_file,
              case_directory_no_index_file,
              case_directory_index_file,
@@ -119,7 +153,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         </html>
         """
 
-    # Handle a GET request.
+    # Classify and handle a GET request.
     def do_GET(self):
         try:
 
@@ -153,16 +187,17 @@ class RequestHandler(BaseHTTPRequestHandler):
             msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
             self.handle_error(msg)
 
-    # Handle file
-    def handle_file(self, full_path):
-        try:
-            # sementara hanya bisa buka send text file
-            with open(full_path, 'r') as reader:
-                content = reader.read()
-            self.send_content(content)
-        except IOError as msg:
-            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
-            self.handle_error(msg)
+    # Run CGI script
+    def run_cgi(self, full_path):
+        '''
+        cmd = "python " + full_path
+        child_stdin, child_stdout = os.popen2(cmd)
+        child_stdin.close()
+        data = child_stdout.read()
+        child_stdout.close()
+        '''
+        data = "Here Python execution's result should be."
+        self.send_content(data)
 
     # Handle unknown objects.
     def handle_error(self, msg):
@@ -181,6 +216,11 @@ class RequestHandler(BaseHTTPRequestHandler):
 if __name__ == '__main__':
     serverAddress = ('', 8060)
     server = HTTPServer(serverAddress, RequestHandler)
+    server.socket = ssl.wrap_socket(server.socket,
+        # keyfile=os.getcwd() + "/key.pem",
+        # certfile=os.getcwd() + "/cert.pem",
+        certfile=os.getcwd() + "/server.pem",
+        server_side=True)
     print("serving at port", 8060)
     server.serve_forever()
     # dz = RequestHandler
